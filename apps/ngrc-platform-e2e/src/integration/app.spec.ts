66bd9de4888aba3dbc50a91576f602787bffb1dc
Felix Lemke <flemke.dev@gmail.com>
2019-10-21 16:10:34 +0200

    ci: add e2e tests

--- apps/ngrc-platform-e2e/src/integration/app.spec.ts
+++ apps/ngrc-platform-e2e/src/integration/app.spec.ts
@@ -1,9 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('ngrc-platform', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to docs!');
+  it('should show the toolbar', () => {
+    cy.get('ngrc-toolbar').should('exist');
   });
 });
