dbd211f4a9242106c7d5f53ef47a72131c9f3fdf
Felix Lemke <flemke.dev@gmail.com>
2019-10-23 16:21:38 +0200

    feat(mappings): add "add-model" if no-model exist

--- apps/ngrc-platform/src/app/containers/mapping-select-dialog/mapping-select-dialog.component.ts
+++ apps/ngrc-platform/src/app/containers/mapping-select-dialog/mapping-select-dialog.component.ts
@@ -1,26 +1,31 @@
-import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Store } from '@ngrx/store';
 import * as fromConfiguration from '../../modules/configuration/+store';
 import { Model, Mapping } from '../../modules/configuration/models';
 import { Observable, Subscription } from 'rxjs';
-import { loadMappings, selectMapping, loadModels } from '../../modules/configuration/+store';
+import { loadMappings, selectMapping, loadModels, selectAllMappings } from '../../modules/configuration/+store';
+import { map } from 'rxjs/operators';
 
 @Component({
   templateUrl: './mapping-select-dialog.component.html',
-  styles: [`mat-form-field { width: 100%; }`]
+  styles: [`
+    mat-form-field { width: 100%; }
+    h3 { margin: 0; }
+    p { margin: 18px 0; }
+  `]
 })
-export class MappingSelectDialogComponent implements OnInit, OnDestroy {
-  title = '';
+export class MappingSelectDialogComponent implements OnInit {
   models$: Observable<Model[]>;
-  mappings: Mapping[];
+  mappings$: Observable<Mapping[]>;
   mapping$: Observable<Mapping>;
   subscription: Subscription;
+  showSelection$: Observable<boolean>;
 
   constructor(
     public dialogRef: MatDialogRef<MappingSelectDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private store: Store<fromConfiguration.State>
+    private store: Store<fromConfiguration.State>,
   ) {}
 
   ngOnInit() {
@@ -28,22 +33,16 @@ export class MappingSelectDialogComponent implements OnInit, OnDestroy {
     this.store.dispatch(loadMappings());
     this.models$ = this.store.select(fromConfiguration.selectAllModels);
     this.mapping$ = this.store.select(fromConfiguration.getSelectedMapping);
-    this.subscription = this.store.select(fromConfiguration.selectAllMappings).subscribe(mappings => {
-      this.mappings = mappings;
-    });
+    this.mappings$ = this.store.select(selectAllMappings);
+    this.showSelection$ = this.models$.pipe(map(models => models.length > 0));
   }
 
-  select(mappingId: number) {
-    const mapping = this.mappings.filter(o => o.id === mappingId)[0];
-    this.store.dispatch(selectMapping({ id: mapping.id }));
+  select(id: number) {
+    this.store.dispatch(selectMapping({ id }));
     this.dialogRef.close();
   }
 
   onNoClick(): void {
     this.dialogRef.close();
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
