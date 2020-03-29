7c5164912ed9a6c2240620d2fc1c54d85fc32299
Felix Lemke <flemke.dev@gmail.com>
2020-03-29 21:29:34 +0200

    feat(app): add socket port 4201 for development

--- apps/ngrc-platform/src/environments/environment.ts
+++ apps/ngrc-platform/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   socket: {
-    baseUrl: 'ws://192.168.178.51:81',
+    baseUrl: 'ws://192.168.178.51:4201',
     config: null
   },
   api: 'http://192.168.178.51:3333/api'
