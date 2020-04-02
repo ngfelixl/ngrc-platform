25cd31a22fe9e8188681795710b1fe628e38f607
Felix Lemke <flemke.dev@gmail.com>
2020-04-02 16:05:58 +0200

    feat(app): refactor add model card

--- apps/ngrc-platform/src/app/modules/configuration/components/model-card/model-card.component.ts
+++ apps/ngrc-platform/src/app/modules/configuration/components/model-card/model-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, HostBinding } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
 
 @Component({
@@ -11,8 +11,10 @@ export class ModelCardComponent {
   environment = environment;
   @Input() logo: string;
   @Input() hint: string;
+  @HostBinding('class.isAdd')
+  @Input() isAdd: boolean;
 
   altImage(img: any) {
-    img.src = './assets/icon-196.png';
+    img.src = this.isAdd ? './assets/model-icons/add.png' : './assets/icon-196.png';
   }
 }
